e48ec6990c2a4935ffc27f0aa484a7193fa02b65
ncastano <nicolas@extratheapp.com>
2024-04-28 09:29:18 -0300

    feat: add more spicy tests.

--- water-jug-challenge/test/waterJug/waterJug.controller.spec.ts
+++ water-jug-challenge/test/waterJug/waterJug.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { WaterJugController } from "../../src/application/controllers/waterJug.controller";
 import { WaterJugService } from "../../src/application/services/waterJugService.service";
+import { RedisModule } from "@nestjs-modules/ioredis";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 describe('WaterJugController', () => {
     let waterJugController: WaterJugController;
@@ -8,6 +10,16 @@ describe('WaterJugController', () => {
   
     beforeEach(async () => {
       const module: TestingModule = await Test.createTestingModule({
+        imports: [
+            RedisModule.forRootAsync({
+                imports: [ConfigModule],
+                inject: [ConfigService],
+                useFactory: (configService: ConfigService) => ({
+                  type: 'single',
+                  url: `redis://${configService.get<string>('REDIS_HOST', 'localhost')}:6379`,
+                }),
+              }),
+        ],
         controllers: [WaterJugController],
         providers: [WaterJugService],
       }).compile();
@@ -43,8 +55,80 @@ describe('WaterJugController', () => {
         expect(solution.some(step => step.bucketX === 2 || step.bucketY === 98)).toBeTruthy();
     });
 
-    it('should not return a solution DFS', async () => {
-        const solution = waterJugService.bfs(3,3,5);
+    it('should not return a solution using DFS', async () => {
+        const solution = waterJugService.dfs(3,3,5);
         expect(solution.length).toBe(0);
+    });
+    
+    it('should return a solution BFS or DFS', async () => {
+        const payload = { x_capacity: 17, y_capacity: 21, z_amount_wanted: 15};
+        const solutionBFS = await waterJugService.solve(payload, 'bfs');
+        expect(solutionBFS.solution[solutionBFS.solution.length-1].status).toEqual('Solved');
+        const solutionDFS = await waterJugService.solve(payload, 'dfs');
+        expect(solutionDFS.solution[solutionDFS.solution.length-1].status).toEqual('Solved');
+    });
+
+    it('should return a solution BFS or DFS', async () => {
+        const payload = { x_capacity: 8, y_capacity: 9, z_amount_wanted: 7};
+        const solutionBFS = await waterJugService.solve(payload, 'bfs');
+        expect(solutionBFS.solution[solutionBFS.solution.length-1].status).toEqual('Solved');
+        const solutionDFS = await waterJugService.solve(payload, 'dfs');
+        expect(solutionDFS.solution[solutionDFS.solution.length-1].status).toEqual('Solved');
+    });
+
+    it('should not return a solution BFS or DFS', async () => {
+        const payload = { x_capacity: 10, y_capacity: 15, z_amount_wanted: 12};
+        const solutionBFS = await waterJugService.solve(payload, 'bfs');
+        expect(solutionBFS.solution[0].status).toEqual('No solution!');
+        const solutionDFS = await waterJugService.solve(payload, 'dfs');
+        expect(solutionBFS.solution[0].status).toEqual('No solution!');
+    });
+
+    it('should return a solution BFS or DFS', async () => {
+        const payload = { x_capacity: 101, y_capacity: 103, z_amount_wanted: 100};
+        const solutionBFS = await waterJugService.solve(payload, 'bfs');
+        expect(solutionBFS.solution[solutionBFS.solution.length-1].status).toEqual('Solved');
+        const solutionDFS = await waterJugService.solve(payload, 'dfs');
+        expect(solutionDFS.solution[solutionDFS.solution.length-1].status).toEqual('Solved');
+    });
+
+    it('should return a solution BFS or DFS', async () => {
+        const payload = { x_capacity: 999, y_capacity: 1000, z_amount_wanted: 500};
+        const solutionBFS = await waterJugService.solve(payload, 'bfs');
+        expect(solutionBFS.solution[solutionBFS.solution.length-1].status).toEqual('Solved');
+        const solutionDFS = await waterJugService.solve(payload, 'dfs');
+        expect(solutionDFS.solution[solutionDFS.solution.length-1].status).toEqual('Solved');
+    });
+
+    it('should not return a solution BFS or DFS', async () => {
+        const payload = { x_capacity: 250, y_capacity: 380, z_amount_wanted: 225};
+        const solutionBFS = await waterJugService.solve(payload, 'bfs');
+        expect(solutionBFS.solution[0].status).toEqual('No solution!');
+        const solutionDFS = await waterJugService.solve(payload, 'dfs');
+        expect(solutionBFS.solution[0].status).toEqual('No solution!');
+    });
+
+    it('should return a solution BFS or DFS', async () => {
+        const payload = { x_capacity: 10000, y_capacity: 15000, z_amount_wanted: 5000};
+        const solutionBFS = await waterJugService.solve(payload, 'bfs');
+        expect(solutionBFS.solution[solutionBFS.solution.length-1].status).toEqual('Solved');
+        const solutionDFS = await waterJugService.solve(payload, 'dfs');
+        expect(solutionDFS.solution[solutionDFS.solution.length-1].status).toEqual('Solved');
+    });
+
+    it('should not return a solution BFS or DFS', async () => {
+        const payload = { x_capacity: 20000, y_capacity: 35000, z_amount_wanted: 1500};
+        const solutionBFS = await waterJugService.solve(payload, 'bfs');
+        expect(solutionBFS.solution[0].status).toEqual('No solution!');
+        const solutionDFS = await waterJugService.solve(payload, 'dfs');
+        expect(solutionBFS.solution[0].status).toEqual('No solution!');
+    });
+
+    it('should not return a solution BFS or DFS', async () => {
+        const payload = { x_capacity: 50000, y_capacity: 100000, z_amount_wanted: 20000};
+        const solutionBFS = await waterJugService.solve(payload, 'bfs');
+        expect(solutionBFS.solution[0].status).toEqual('No solution!');
+        const solutionDFS = await waterJugService.solve(payload, 'dfs');
+        expect(solutionBFS.solution[0].status).toEqual('No solution!');
     });
   });
